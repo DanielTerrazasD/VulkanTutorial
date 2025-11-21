b8bf46fc2f4f476ed6c7762fa3f8358dd5dca030
Daniel Terrazas <daniel.terrazas.delgado@gmail.com>
2025-11-21 16:48:23 -0600

    01_instance_creation

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
@@ -21,28 +22,36 @@ public:
     }
 
 private:
-    GLFWwindow* window;
+    GLFWwindow* mWindow;
+
+    VkInstance mInstance;
 
     /**
-     * @brief Initialize GLFW Window
+     * @brief GLFW Window Initialization.
      * 
      */
     void InitWindow();
 
     /**
-     * @brief Vulkan Initialization
+     * @brief Vulkan Initialization.
      * 
      */
     void InitVulkan();
 
     /**
-     * @brief 
+     * @brief Create and initialize a Vulkan Instance object
+     * 
+     */
+    void CreateInstance();
+
+    /**
+     * @brief Application running loop.
      * 
      */
     void MainLoop();
 
     /**
-     * @brief 
+     * @brief Cleanup procedure.
      * 
      */
     void Cleanup();
@@ -55,19 +64,67 @@ void HelloTriangleApp::InitWindow()
     // Since GLFW was designed to create an OpenGL context, we need to specify to
     // not create an OpenGL context (GLFW_NO_API) with the following call:
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    // Disabling resizing for now.
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    mWindow = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
 }
 
 void HelloTriangleApp::InitVulkan()
 {
+    CreateInstance();
+}
 
+void HelloTriangleApp::CreateInstance()
+{
+    // VkApplicationInfo struct:
+    // Technically optional but provide useful information to the driver.
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Hello Triangle";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    // VkInstanceCreateInfo struct:
+    // Required by the driver to specify which global extensions and validation layers we want to use.
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    // Vulkan is a platform agnostic API, which means we need an extension to interface with the window system.
+    // GLFW has a built-in function that returns the extension(s) it needs to do that.
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+    // The last two members of the struct determine the global validation layers to enable.
+    // Leaving empty for now.
+    createInfo.enabledLayerCount = 0;
+    // createInfo.ppEnabledLayerNames;
+
+    // To retrieve a list of supported extensions before creating an instance use vkEnumerateInstanceExtensionProperties.
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    std::vector<VkExtensionProperties> extensions(extensionCount);
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+    std::cout << "Available Extensions:\n";
+    for (const auto& extension : extensions)
+        std::cout << "    " << extension.extensionName << '\n';
+
+    // Issue the Vulkan create instance call and check for errors:
+    if (vkCreateInstance(&createInfo, nullptr, &mInstance) != VK_SUCCESS)
+    {
+        throw std::runtime_error("Failed to create instance!");
+    }
 }
 
 void HelloTriangleApp::MainLoop()
 {
-    while (!glfwWindowShouldClose(window))
+    while (!glfwWindowShouldClose(mWindow))
     {
         glfwPollEvents();
     }
@@ -75,7 +132,10 @@ void HelloTriangleApp::MainLoop()
 
 void HelloTriangleApp::Cleanup()
 {
-    glfwDestroyWindow(window);
+    // VkInstance should be only destroyed right before the program exits.
+    vkDestroyInstance(mInstance, nullptr);
+
+    glfwDestroyWindow(mWindow);
 
     glfwTerminate();
 }
